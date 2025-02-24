71154ce3efbbe2c3f158d627c981064a7af0cabe
BugHunter-X <askzenai@gmail.com>
2025-02-24 09:54:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,30 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* The order matters! Catch-all routes should always be last. */}
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function NoMatch() {
+  return <h1>404 Not Found</h1>;
+}
+
+export default App;
+```
